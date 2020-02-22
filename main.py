57512d35403ebcd6bba9569c63ed6acac0e1ce94
Артем Апалько <a.apalko@cian.ru>
2020-02-22 15:47:42 +0300

    Player name, enums

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+from enum import Enum
 from typing import Tuple, List
 
 SIZE = 10, 10
@@ -16,33 +17,49 @@ SHIPS_SIZE_DICT = {
 }
 
 
+class HitStatus(Enum):
+    HAS_HIT = -2
+    MISS = -1
+    HIT = 0
+    KILLED = 1
+
+
+LEGEND = {
+    'EMPTY': ' ',
+    'MISS': '0',
+    'SHIP': '=',
+    'HIT': '#',
+}
+
+
 class Ship:
-    def __init__(self, size: int, coordinates: List[Tuple[int, int]]):
+    def __init__(self, *, size: int, coordinates: List[Tuple[int, int]]):
         self.size = size,
         self.coordinates = coordinates
         self.hits = 0
 
 
 class BattleshipBoard:
-    def __init__(self, *, height: int=SIZE[0], width: int=SIZE[1]):
+    def __init__(self, *, name: str, height: int = SIZE[0], width: int = SIZE[1]):
+        self.name = name
         self._ships = []
         self.height = height - 1
         self.width = width - 1
-        self.board = [[' ' for _ in range(height)] for _ in range(width)]
-        self.enemy_board = [[' ' for _ in range(height)] for _ in range(width)]
+        self.board = [[LEGEND['EMPTY'] for _ in range(height)] for _ in range(width)]
+        self.enemy_board = [[LEGEND['EMPTY'] for _ in range(height)] for _ in range(width)]
         self.ships_count = 0
 
         for ship, count in SHIPS_COUNT_DICT.items():
             for x in range(count):
                 self.set_ship(ship_size=SHIPS_SIZE_DICT[ship])
 
-    def set_ship(self, *, ship_size: int) -> None:
+    def set_ship(self, ship_size: int) -> None:
         founded: List[Tuple[int, int]] = []
 
         while True:
             if len(founded) == ship_size:
                 for n_x, n_y in founded:
-                    self.board[n_x][n_y] = '='
+                    self.board[n_x][n_y] = LEGEND['SHIP']
                     self.ships_count += 1
 
                 return
@@ -57,13 +74,13 @@ class BattleshipBoard:
             tilt = random.randint(0, 1)
             if tilt == 0:
                 while desired_size > 0:
-                    for i in range(x, x+ship_size):
+                    for i in range(x, x + ship_size):
                         if self.has_neighbors(x=i, y=y):
                             break
-                        founded.append((i,y))
+                        founded.append((i, y))
                         desired_size -= 1
 
-                    for i in range(x-ship_size, x-1):
+                    for i in range(x - ship_size, x - 1):
                         if self.has_neighbors(x=i, y=y):
                             break
                         founded.append((i, y))
@@ -72,14 +89,14 @@ class BattleshipBoard:
                     break
 
             elif tilt == 1:
-                for i in range(y, y+ship_size):
+                for i in range(y, y + ship_size):
                     if desired_size > 0:
                         if self.has_neighbors(x=x, y=i):
                             break
                         founded.append((x, i))
                         desired_size -= 1
 
-                for i in range(y-ship_size, y-1):
+                for i in range(y - ship_size, y - 1):
                     if desired_size > 0:
                         if self.has_neighbors(x=x, y=i):
                             break
@@ -89,30 +106,33 @@ class BattleshipBoard:
 
                     break
 
-    def check_hit(self, x, y) -> int:
+    def check_hit(self, x, y) -> HitStatus:
         if x < 0 or y < 0:
-            return -1
+            return HitStatus.MISS
 
         if x > self.height or y > self.width:
-            return -1
+            return HitStatus.MISS
 
         val = self.get_value(x, y)
-        if val != '=':
-            return -1
+        if val == LEGEND['HIT']:
+            return HitStatus.HAS_HIT
+
+        if val != LEGEND['SHIP']:
+            return HitStatus.MISS
 
         self.ships_count -= 1
-        self.board[int(x)][int(y)] = '#'
+        self.board[int(x)][int(y)] = LEGEND['HIT']
 
-        if not self.has_neighbors(x=x,y=y):
-            return 1
+        if not self.has_neighbors(x=x, y=y):
+            return HitStatus.KILLED
 
-        return 0
+        return HitStatus.HIT
 
     def get_value(self, x: int, y: int) -> str:
         if x < 0 or y < 0:
-            return ' '
+            return LEGEND['EMPTY']
         if x > self.height or y > self.width:
-            return ' '
+            return LEGEND['EMPTY']
 
         return self.board[x][y]
 
@@ -123,35 +143,35 @@ class BattleshipBoard:
         if x > self.height or y > self.width:
             return True
 
-        if self.get_value(x, y) == '=':
+        if self.get_value(x, y) == LEGEND['SHIP']:
             return True
 
         if x - 1 >= 0:
             if y - 1 >= 0:
-                if self.get_value(x - 1, y - 1) == '=':
+                if self.get_value(x - 1, y - 1) == LEGEND['SHIP']:
                     return True
-            if self.get_value(x - 1, y) == '=':
+            if self.get_value(x - 1, y) == LEGEND['SHIP']:
                 return True
 
         if x + 1 < self.height:
             if y + 1 < self.width:
-                if self.get_value(x + 1, y + 1) == '=':
+                if self.get_value(x + 1, y + 1) == LEGEND['SHIP']:
                     return True
-            if self.get_value(x + 1, y) == '=':
+            if self.get_value(x + 1, y) == LEGEND['SHIP']:
                 return True
 
         if y + 1 < self.width:
-            if self.get_value(x, y + 1) == '=':
+            if self.get_value(x, y + 1) == LEGEND['SHIP']:
                 return True
             if x - 1 >= 0:
-                if self.get_value(x-1, y + 1) == '=':
+                if self.get_value(x - 1, y + 1) == LEGEND['SHIP']:
                     return True
 
         if x + 1 < self.height:
-            if self.get_value(x + 1, y) == '=':
+            if self.get_value(x + 1, y) == LEGEND['SHIP']:
                 return True
             if y - 1 >= 0:
-                if self.get_value(x + 1, y - 1) == '=':
+                if self.get_value(x + 1, y - 1) == LEGEND['SHIP']:
                     return True
 
         return False
@@ -161,59 +181,69 @@ class BattleshipBoard:
             print(self.board[i], '      |      ', self.enemy_board[i])
 
 
+def get_input(player_name: str) -> Tuple[int, int]:
+    cells = input(f'{player_name}\'s turn: X:Y').split(':')
+
+    x = ord(cells[0].lower()) - 97
+
+    return x, int(cells[1]) - 1
+
+
 def main():
-    player_1 = BattleshipBoard()
-    player_2 = BattleshipBoard()
+    player_1 = BattleshipBoard(name=input('Player 1 name:'))
+    player_2 = BattleshipBoard(name=input('Player 2 name:'))
+
     while player_1.ships_count > 0 or player_2.ships_count > 0:
         while True:
-            print('Player1 turn:')
-            # x, y = input('Player1 turn:')
             player_1.print_board()
-            x = random.randint(0, player_1.height)
-            y = random.randint(0, player_1.width)
-            print(x ,y)
-            has_hit = player_2.check_hit(int(x), int(y))
-            if has_hit == -1:
-                player_1.enemy_board[int(x)][int(y)] = 'O'
-                print('Miss!')
-                break
+            # x, y = get_input(player_1.name)
 
-            player_1.enemy_board[int(x)][int(y)] = '#'
-
-            if has_hit == 1:
-                print('Killed!')
-            else:
-                print('Hit!')
-
-            if player_2.ships_count == 0:
-                print('Player1 wins!')
-                return
-
-        while True:
-            # x, y = input('Player2 turn:')
-            print('Player2 turn:')
-            player_2.print_board()
             x = random.randint(0, player_1.height)
             y = random.randint(0, player_1.width)
             print(x, y)
-            has_hit = player_1.check_hit(int(x), int(y))
-            if has_hit == -1:
-                player_2.enemy_board[int(x)][int(y)] = 'O'
+
+            has_hit = player_2.check_hit(int(x), int(y))
+            if has_hit == HitStatus.MISS:
+                player_1.enemy_board[int(x)][int(y)] = LEGEND['MISS']
                 print('Miss!')
                 break
+            if has_hit in (HitStatus.HIT, HitStatus.KILLED):
+                player_1.enemy_board[int(x)][int(y)] = LEGEND['HIT']
 
-            player_2.enemy_board[int(x)][int(y)] = '#'
+                if has_hit == HitStatus.KILLED:
+                    print('Killed!')
+                else:
+                    print('Hit!')
 
-            if has_hit == 1:
-                print('Killed!')
-            else:
-                print('Hit!')
+                if player_2.ships_count == 0:
+                    print(f'{player_1.name} wins!')
+                    return
+
+        while True:
+            player_2.print_board()
+            # x, y = get_input(player_2.name)
+
+            x = random.randint(0, player_1.height)
+            y = random.randint(0, player_1.width)
+            print(x, y)
+
+            has_hit = player_1.check_hit(int(x), int(y))
+            if has_hit == HitStatus.MISS:
+                player_2.enemy_board[int(x)][int(y)] = LEGEND['MISS']
+                print('Miss!')
+                break
+            if has_hit in (HitStatus.HIT, HitStatus.KILLED):
+                player_2.enemy_board[int(x)][int(y)] = LEGEND['HIT']
+
+                if has_hit == HitStatus.KILLED:
+                    print('Killed!')
+                else:
+                    print('Hit!')
 
             if player_1.ships_count == 0:
-                print('Player1 wins!')
+                print(f'{player_1.name} wins!')
                 return
 
-    print('END')
 
 if __name__ == "__main__":
     main()
